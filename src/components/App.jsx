052d80cc4c5abba9e4ff6cfdc0b5c1b657e5dc1e
unknown <poputa.alex@gmail.com>
2024-10-24 15:28:17 +0300

    declared state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,99 +2,96 @@ import axios from 'axios';
 import ImageGallery from './ImageGallery';
 import Loader from './Loader';
 import SearchBar from './SearchBar';
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import Modal from './Modal';
 
 const END_POINT = 'https://pixabay.com/api/';
-// eslint-disable-next-line react-refresh/only-export-components
 const KEY = '45639968-4ab0c3e34d3afa9a12b28af2f';
 
-export default class App extends Component {
-	async componentDidUpdate(prevProps, prevState) {
-		if (
-			prevState.filter !== this.state.filter ||
-			prevState.page !== this.state.page
-		) {
-			try {
-				this.handleLoader();
-				const response = await axios.get(
-					`${END_POINT}?key=${KEY}&q=${this.state.filter}&safesearch=true&page=${this.state.page}&per_page=${this.state.per_page}`
-				);
-				this.setState({
-					images: [...this.state.images, ...response.data.hits],
-				});
-			} catch (err) {
-				console.error(err);
-			} finally {
-				this.handleLoader();
-			}
+const App =()=> {
+
+	const [images,setImages]=useState([])
+	const [query,setQuery]=useState('')
+	const [modalIsOpen, setModalOpen]=useState(false)
+	const [modalImg, setModalImg]=useState('')
+	const [isLoading,setIsLoading]=useState(false)
+	const [page,setPage]=useState(1)
+	const [per_page,setPerPage]=useState(12)
+
+	useEffect(()=>{
+		try {
+			handleLoader();
+			
+			setImages([...images,...handleFetch()]);
+		} catch (err) {
+			console.error(err);
+		} finally {
+			handleLoader();
 		}
+	},[query,page])
+
+
+	const handleFetch=async()=>{
+		const response = await axios.get(
+			`${END_POINT}?key=${KEY}&q=${query}&safesearch=true&page=${page}&per_page=${per_page}`
+		);
+		return response.data.hits
 	}
+	
 
-	state = {
-		images: [],
-		filter: '',
-		hasError: false,
-		modalIsOpen: false,
-		modalImg: '',
-		isLoading: false,
-		page: 1,
-		per_page: 12,
+	const handleLoader = () => {
+		setIsLoading(!isLoading)
 	};
 
-	handleLoader = () => {
-		this.setState((prevState) => ({ isLoading: !prevState.isLoading }));
+	const handleModal = () => {
+		setModalOpen(!modalIsOpen)
 	};
 
-	handleModal = () => {
-		this.setState((prevState) => ({ modalIsOpen: !prevState.modalIsOpen }));
+	const handleSearch = (data) => {
+		setImages([])
+		setPage(1)
+		setQuery(data);
 	};
 
-	handleSearch = (data) => {
-		this.setState({ images: [] });
-		this.setState({ page: 1 });
-		this.setState({ filter: data });
+	const handleImages = (data) => {
+		setImages(data);
 	};
 
-	handleImages = (data) => {
-		this.setState({ images: data });
+	const loadMore = () => {
+		setPage(page);
 	};
 
-	loadMore = () => {
-		this.setState((prevState) => ({ page: prevState.page + 1 }));
+	const handleModalImg = (data) => {
+		setModalImg(data)
 	};
 
-	setModalImg = (data) => {
-		this.setState({ modalImg: data });
-	};
-
-	render() {
 		return (
 			<>
 				<SearchBar
-					onSearch={this.handleSearch}
-					onHandleImages={this.handleImages}
-					getImages={this.getImages}
-					page={this.state.page}
-					per_page={this.state.per_page}
+					onSearch={handleSearch}
+					onHandleImages={handleImages}
+					getImages={getImages}
+					page={page}
+					per_page={per_page}
 				/>
-				<Loader loading={this.state.isLoading} />
-				{this.state.images.length > 0 ? (
+				<Loader loading={isLoading} />
+				{images.length > 0 ? (
 					<ImageGallery
-						images={this.state.images}
-						loadMore={this.loadMore}
-						handleModal={this.handleModal}
-						setModalImg={this.setModalImg}
+						images={images}
+						loadMore={loadMore}
+						handleModal={handleModal}
+						setModalImg={handleModalImg}
 					/>
 				) : (
 					<h1 style={{textAlign:'center', marginTop:'10px'}}>Use the search bar to find images </h1>
 				)}
 				<Modal
-					selectedImg={this.state.modalImg}
-					modalIsOpen={this.state.modalIsOpen}
-					handleModal={this.handleModal}
+					selectedImg={modalImg}
+					modalIsOpen={modalIsOpen}
+					handleModal={handleModal}
 				/>
 			</>
 		);
 	}
-}
+
+export default App
