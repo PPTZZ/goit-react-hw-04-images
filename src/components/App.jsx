b6e5c2c13b19b602d079cb91b85b846411e81ff9
unknown <poputa.alex@gmail.com>
2024-10-24 15:51:06 +0300

    declared state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,48 +8,43 @@ import Modal from './Modal';
 const END_POINT = 'https://pixabay.com/api/';
 const KEY = '45639968-4ab0c3e34d3afa9a12b28af2f';
 
-const App =()=> {
+const App = () => {
+	const [images, setImages] = useState([]);
+	const [query, setQuery] = useState('');
+	const [modalIsOpen, setModalOpen] = useState(false);
+	const [modalImg, setModalImg] = useState('');
+	const [isLoading, setIsLoading] = useState(false);
+	const [page, setPage] = useState(1);
+	const [per_page] = useState(12);
 
-	const [images,setImages]=useState([])
-	const [query,setQuery]=useState('')
-	const [modalIsOpen, setModalOpen]=useState(false)
-	const [modalImg, setModalImg]=useState('')
-	const [isLoading,setIsLoading]=useState(false)
-	const [page,setPage]=useState(1)
-	const [per_page,setPerPage]=useState(12)
-
-	useEffect(()=>{
-		try {
-			handleLoader();
-			
-			setImages([...images,...handleFetch()]);
-		} catch (err) {
-			console.error(err);
-		} finally {
-			handleLoader();
-		}
-	},[query,page])
-
-
-	const handleFetch=async()=>{
-		const response = await axios.get(
-			`${END_POINT}?key=${KEY}&q=${query}&safesearch=true&page=${page}&per_page=${per_page}`
-		);
-		return response.data.hits
-	}
 	
+		const handleFetch = async () => {
+			try{
+				handleLoader()
+				const response = await axios.get(
+					`${END_POINT}?key=${KEY}&q=${query}&safesearch=true&page=${page}&per_page=${per_page}`
+				);
+				return response.data.hits;
+			}catch(err){
+				console.error(err)
+			}finally{
+				handleLoader()
+			}
+		
+
+
 
 	const handleLoader = () => {
-		setIsLoading(!isLoading)
+		setIsLoading(!isLoading);
 	};
 
 	const handleModal = () => {
-		setModalOpen(!modalIsOpen)
+		setModalOpen(!modalIsOpen);
 	};
 
 	const handleSearch = (data) => {
-		setImages([])
-		setPage(1)
+		setImages([]);
+		setPage(1);
 		setQuery(data);
 	};
 
@@ -62,36 +57,32 @@ const App =()=> {
 	};
 
 	const handleModalImg = (data) => {
-		setModalImg(data)
+		setModalImg(data);
 	};
 
-		return (
-			<>
-				<SearchBar
-					onSearch={handleSearch}
-					onHandleImages={handleImages}
-					getImages={getImages}
-					page={page}
-					per_page={per_page}
-				/>
-				<Loader loading={isLoading} />
-				{images.length > 0 ? (
-					<ImageGallery
-						images={images}
-						loadMore={loadMore}
-						handleModal={handleModal}
-						setModalImg={handleModalImg}
-					/>
-				) : (
-					<h1 style={{textAlign:'center', marginTop:'10px'}}>Use the search bar to find images </h1>
-				)}
-				<Modal
-					selectedImg={modalImg}
-					modalIsOpen={modalIsOpen}
+	return (
+		<>
+			<SearchBar onSearch={handleSearch} onHandleImages={handleImages} />
+			<Loader loading={isLoading} />
+			{images.length > 0 ? (
+				<ImageGallery
+					images={images}
+					loadMore={loadMore}
 					handleModal={handleModal}
+					setModalImg={handleModalImg}
 				/>
-			</>
-		);
-	}
+			) : (
+				<h1 style={{ textAlign: 'center', marginTop: '10px' }}>
+					Use the search bar to find images{' '}
+				</h1>
+			)}
+			<Modal
+				selectedImg={modalImg}
+				modalIsOpen={modalIsOpen}
+				handleModal={handleModal}
+			/>
+		</>
+	);
+};
 
-export default App
+export default App;
--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,67 +1,60 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import SearchIcon from '@mui/icons-material/Search';
 import { AppBar, InputAdornment, TextField } from '@mui/material';
 
-export default class SearchBar extends Component {
-  state = {
-    searchWord: '',
-  };
+const SearchBar = ({ onSearch }) => {
+	const [searchWord, setSearchWord] = useState('');
 
-  handleKeydown=(e)=> {
-    if(e.code !== 'Enter'){
-      return
-    }
-    this.setState({searchWord:''});
-    this.setState({searchWord:e.target.value});
-    
-  }
-  componentDidUpdate(prevProps,prevState){
-    if (prevState.searchWord !== this.state.searchWord){
-      this.props.onSearch(this.state.searchWord)
-    }
-  }
+	useEffect(() => {
+		onSearch(searchWord);
+	}, [searchWord]);
 
-  render() {
-    return (
-      <AppBar
-        position='static'
-        sx={{ height: 70, alignItems: 'center', justifyContent: 'center' }}>
-        <TextField
-          onKeyDown={this.handleKeydown}
-          variant='outlined'
-          placeholder='Search'
-          sx={{
-            width: 250,
-            '& .MuiOutlinedInput-root': {
-              color: 'text.primary',
-              backgroundColor: '#fff',
-              fontWeight: 'bold',
-              '& .MuiOutlinedInput-notchedOutline': {
-                borderColor: '#2e2e2e',
-                borderWidth: '2px',
-              },
-            },
-          }}
-          slotProps={{
-            input: {
-              startAdornment: (
-                <InputAdornment position='start'>
-                  <SearchIcon />
-                </InputAdornment>
-              ),
-            },
-          }}
-        />
-      </AppBar>
-    );
-  }
-}
+	const handleKeydown = (e) => {
+		if (e.code !== 'Enter') {
+			return;
+		}
+		setSearchWord(e.target.value);
+	};
+
+	return (
+		<AppBar
+			position='static'
+			sx={{ height: 70, alignItems: 'center', justifyContent: 'center' }}
+		>
+			<TextField
+				onKeyDown={handleKeydown}
+				variant='outlined'
+				placeholder='Search'
+				sx={{
+					width: 250,
+					'& .MuiOutlinedInput-root': {
+						color: 'text.primary',
+						backgroundColor: '#fff',
+						fontWeight: 'bold',
+						'& .MuiOutlinedInput-notchedOutline': {
+							borderColor: '#2e2e2e',
+							borderWidth: '2px',
+						},
+					},
+				}}
+				slotProps={{
+					input: {
+						startAdornment: (
+							<InputAdornment position='start'>
+								<SearchIcon />
+							</InputAdornment>
+						),
+					},
+				}}
+			/>
+		</AppBar>
+	);
+};
 
 SearchBar.propTypes = {
-  onSearch: PropTypes.func,
-  page: PropTypes.number,
-  per_page: PropTypes.number,
-  getImages: PropTypes.func,
-  onHandleImages: PropTypes.func,
+	onSearch: PropTypes.func,
+	onHandleImages: PropTypes.func,
 };
+
+export default SearchBar
